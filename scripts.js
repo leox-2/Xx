787a6b199c8930499620d7ef6ad0d3d2501f31ff
leox-2 <168227748+leox-2@users.noreply.github.com>
2024-07-15 10:43:00 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
-document.getElementById('runCode').addEventListener('click', () => {
-    const htmlCode = document.getElementById('html').value;
-    const cssCode = `<style>${document.getElementById('css').value}</style>`;
-    const jsCode = `<script>${document.getElementById('js').value}<\/script>`;
-    
-    const outputFrame = document.getElementById('output');
-    const outputDocument = outputFrame.contentDocument || outputFrame.contentWindow.document;
-    outputDocument.open();
-    outputDocument.write(htmlCode + cssCode + jsCode);
-    outputDocument.close();
+document.getElementById('run-code').addEventListener('click', function() {
+    var htmlCode = document.getElementById('html-code').value;
+    var cssCode = document.getElementById('css-code').value;
+    var jsCode = document.getElementById('js-code').value;
+
+    var resultFrame = document.getElementById('result-frame');
+    resultFrame.contentDocument.body.innerHTML = htmlCode + '<style>' + cssCode + '</style>';
+    resultFrame.contentWindow.eval(jsCode);
+});
+
+document.getElementById('download-project').addEventListener('click', function() {
+    var htmlCode = document.getElementById('html-code').value;
+    var cssCode = document.getElementById('css-code').value;
+    var jsCode = document.getElementById('js-code').value;
+
+    var blob = new Blob([htmlCode + '\n<style>' + cssCode + '</style>\n<script>' + jsCode + '</script>'], { type: 'text/html' });
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'project.html';
+    link.click();
 });
